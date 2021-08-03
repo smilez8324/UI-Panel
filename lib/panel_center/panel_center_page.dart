658c946243806e9826ab416ee0d9899510087d4b
IncogKneeGrow <smilez8324@gmail.com>
2021-08-03 00:05:28 -0700

    I am at 38.01 in the video.

--- lib/panel_center/panel_center_page.dart
+++ lib/panel_center/panel_center_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:uipanel/constants.dart';
+
+class Person {
+  String name;
+  Color color;
+
+  Person({required this.name, required this.color});
+}
 
 class PanelCenterPage extends StatefulWidget {
   @override
@@ -6,11 +14,105 @@ class PanelCenterPage extends StatefulWidget {
 }
 
 class _PanelLeftPageState extends State<PanelCenterPage> {
+  List<Person> _persons = [
+    Person(name: "Theia Brown", color: Constants.orangeLight),
+    Person(name: "Fariha Odling", color: Constants.redLight),
+    Person(name: "Viola Willis", color: Constants.blueLight),
+    Person(name: "Emmett Forrest", color: Constants.orangeLight),
+    Person(name: "Nick Jarvis", color: Constants.greenLight),
+    Person(name: "Amit Clayeia", color: Constants.orangeLight),
+    Person(name: "Amalie Howardeia", color: Constants.redLight),
+    Person(name: "Campbell Britton", color: Constants.blueLight),
+    Person(name: "Haley Mellor", color: Constants.redLight),
+    Person(name: "Harlen Higgins", color: Constants.greenLight),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: Constants.kPadding / 2,
+                right: Constants.kPadding / 2,
+                top: Constants.kPadding / 2,
+              ),
+              child: Card(
+                color: Constants.purpleLight,
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Container(
+                  width: double.infinity,
+                  child: ListTile(
+                      title: Text(
+                        "Products Available",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      subtitle: Text(
+                        "82% of the Products Avail.",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      trailing: Chip(
+                        label: Text(
+                          "20,500",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      )),
+                ),
+              ),
+            ),
+            //Middle Chart
+            Padding(
+              padding: const EdgeInsets.only(
+                bottom: Constants.kPadding,
+                left: Constants.kPadding / 2,
+                right: Constants.kPadding / 2,
+                top: Constants.kPadding,
+              ),
+              child: Card(
+                color: Constants.purpleLight,
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  children: List.generate(
+                    _persons.length,
+                    (index) => ListTile(
+                      leading: CircleAvatar(
+                        radius: 15,
+                        child: Text(
+                          _persons[index].name.substring(0, 1),
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        backgroundColor: _persons[index].color,
+                      ),
+                      title: Text(
+                        _persons[index].name,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      trailing: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.message,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
